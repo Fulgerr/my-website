e74716d0964d2dc5e8c42a266de831d12881ab2d
Fulger <petrina.calota@uipath.com>
2025-08-30 22:13:22 +0300

    add key

--- src/hooks/useAptabase.js
+++ src/hooks/useAptabase.js
@@ -5,7 +5,7 @@ export const useAptabase = () => {
   useEffect(() => {
     // Only initialize on the client side
     if (typeof window !== 'undefined') {
-      init('YOUR_APTABASE_APP_KEY', {
+      init('A-EU-5971610612', {
         // Optional configuration
         host: 'https://eu.aptabase.com', // or your region-specific endpoint
         isDebug: process.env.NODE_ENV !== 'production', // Enable debug logs in development
